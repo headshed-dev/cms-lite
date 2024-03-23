f4da6ac447cd62d8d414ebe0a397f51783431f16
jon brookes <marshyon@gmail.com>
2024-03-23 12:50:22 +0000

    add job and move work from command to job

--- laravel-app/app/Console/Commands/ExportBlogArticles.php
+++ laravel-app/app/Console/Commands/ExportBlogArticles.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Jobs\ExportBlogArticlesJob;
 
 class ExportBlogArticles extends Command
 {
@@ -25,61 +26,6 @@ class ExportBlogArticles extends Command
      */
     public function handle()
     {
-        $blogs = \App\Models\Blog::all();
-
-        $this->info('Exporting blog articles...');
-
-
-        $directoryPath = storage_path('app/exports/blogs');
-
-        if (!file_exists($directoryPath)) {
-            mkdir($directoryPath, 0775, true);
-        }
-
-        $blogs->each(function ($blog) {
-
-            $blogData = [
-                'title' => $blog->title,
-                'slug' => $blog->slug,
-                'description' => $blog->description,
-                'keywords' => $blog->keywords,
-                'tags' => $blog->tags,
-            ];
-
-            $blogHeadMatter = \Symfony\Component\Yaml\Yaml::dump($blogData);
-
-            $blog_file_path = storage_path('app/exports/blogs/' . $blog->slug . '.md');
-
-            $this->info('Exporting blog article: ' . $blog_file_path);
-
-            $blog_markdown = "---\n" . $blogHeadMatter . "---\n\n" . $blog->content;
-
-            file_put_contents($blog_file_path, $blog_markdown);
-        });
-
-
-
-        $settings = \App\Models\Setting::all();
-
-        $settingsData = [];
-        $settings->each(function ($setting) use (&$settingsData) {
-
-            if ($setting->value) {
-                $settingsData[$setting->label] = $setting->value;
-            }
-        });
-
-        $settingsJson = json_encode($settingsData);
-
-        $settingsDirectoryPath = storage_path('app/exports/settings');
-
-        if (!file_exists($settingsDirectoryPath)) {
-            mkdir($settingsDirectoryPath, 0775, true);
-        }
-        file_put_contents($settingsDirectoryPath . '/settings.json', $settingsJson);
-
-        $this->info('Settings exported to ' . $settingsDirectoryPath . '/settings.json successfully!');
-
-        $this->info('Blog articles and settings export completed');
+        ExportBlogArticlesJob::dispatch()->onQueue('export_blog_articles');
     }
 }
