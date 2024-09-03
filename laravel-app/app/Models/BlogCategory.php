2e3db0bafd425199bcff9399682d5e355819e5c3
Jon Brookes <marshyon@gmail.com>
2024-09-03 10:21:34 +0100

    add auditable to models

--- laravel-app/app/Models/BlogCategory.php
+++ laravel-app/app/Models/BlogCategory.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class BlogCategory extends Model
+class BlogCategory extends Model implements Auditable
 {
     use HasFactory;
-
+    use \OwenIt\Auditing\Auditable;
+    
     protected $fillable = [
         "name",
     ];
--- laravel-app/app/Models/ImageCategory.php
+++ laravel-app/app/Models/ImageCategory.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class ImageCategory extends Model
+class ImageCategory extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
+    
     protected $fillable = [
         'name'
     ];
