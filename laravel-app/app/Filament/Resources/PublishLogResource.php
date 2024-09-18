2afa5bc9b5a583010864cf71cb363cda59e69f64
Jon Brookes <marshyon@gmail.com>
2024-09-18 13:01:36 +0100

    tweak icons

--- laravel-app/app/Filament/Resources/PublishLogResource.php
+++ laravel-app/app/Filament/Resources/PublishLogResource.php
@@ -20,7 +20,7 @@ class PublishLogResource extends Resource
 {
     protected static ?string $model = PublishLog::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-paper-airplane';
 
     protected static ?string $navigationGroup = 'Configuration';
 
